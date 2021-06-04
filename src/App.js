ad12589acc4beaf4a54359c197ceea008cac3fd1
太田有紀 <ota.yuki@lancers.co.jp>
2021-06-05 00:42:25 +0900

    statement

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // DO NOT DELETE
-
 import * as React from 'react'
+import { useState } from 'react';
 import './App.css'
-
 /**
  *
  * @type {React.FC}
  */
-export const App = () => {
+export function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [dogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
   return (
     <header>
       <h1>Dogアプリ</h1>
       <p>説明文</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+      <img src={dogUrl}></img>
     </header>
   )
 }
