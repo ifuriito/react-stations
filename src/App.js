bd1977f9dd3f27a9b24409b0e1c8edbc77df575d
太田有紀 <ota.yuki@lancers.co.jp>
2021-06-09 01:10:57 +0900

    ランダムで犬画像を取得する処理

--- src/App.js
+++ src/App.js
@@ -9,12 +9,21 @@ import './App.css'
 export function App() {
   // Declare a new state variable, which we'll call "count"
   const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+
+  async function getDogUrl() {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await res.json();
+    const url = JSON.stringify(data);
+    const dogJson = JSON.parse(url);
+    setDogUrl(dogJson.message);
+  }
+
   return (
     <header>
       <h1>Dogアプリ</h1>
       <p>説明文</p>
       <img src={dogUrl}></img>
-      <button onClick={() => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>
+      <button onClick={() => getDogUrl()}>
         更新
       </button>
     </header>
