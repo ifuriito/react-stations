aa0f328dde7bb5b376317d033164aed12c1ef3a5
太田有紀 <ota.yuki@lancers.co.jp>
2021-06-12 20:47:55 +0900

    不要なコード消去

--- src/Header.js
+++ src/Header.js
@@ -7,16 +7,6 @@ import './App.css'
  * @type {React.FC}
  */
 export function Header() {
-    // Declare a new state variable, which we'll call "count"
-    const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
-
-    async function getDogUrl() {
-        const res = await fetch('https://dog.ceo/api/breeds/image/random');
-        const data = await res.json();
-        const url = JSON.stringify(data);
-        const dogJson = JSON.parse(url);
-        setDogUrl(dogJson.message);
-    }
 
     return (
         <header>
